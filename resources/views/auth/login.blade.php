9ecb06a493a2415f48b493344b7236ac5df4d491
danuartha <danuartha.10109013@student.polsub.ac.id>
2024-09-13 11:33:22 +0700

    logoin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="apple-touch-icon" sizes="76x76" href="{{asset('vendor')}}/assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="{{asset('vendor')}}/assets/img/favicon.png">
+  <link rel="icon" type="image/png" href="{{asset('PT. Bersama Sahabat Makmur Logo.png')}}">
   <title>Login || PT. Bersama Sahabat Makmur</title>
 
   <!-- Fonts and icons -->
